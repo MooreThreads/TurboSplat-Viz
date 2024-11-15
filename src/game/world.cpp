c77e64ca4f3b997ae2cda2d976723a0da1a645de
KaiminLiao <kaimin.liao@mthreads.com>
2024-11-15 15:22:22 +0800

    skip loading the 3d-gaussian scene

--- src/game/world.cpp
+++ src/game/world.cpp
@@ -178,8 +178,8 @@ void TestWorld::Init()
 	//create objs
 	/*auto obj1 = std::make_shared<StaticMesh>(shared_from_this(), DirectX::XMFLOAT3{0.0f,0.0f,1.0f}, DirectX::XMFLOAT3{1.0f,1.0f,1.0f}, DirectX::XMFLOAT3{0.0f,0.0f,0.0f});
 	obj1->Init();*/
-	auto obj2 = std::make_shared<GaussianPoints>(shared_from_this(), DirectX::XMFLOAT3{0.0f,0.0f,0.0f}, DirectX::XMFLOAT3{1.0f,1.0f,1.0f}, DirectX::XMFLOAT3{0.0f,0.0f,0.0f}, "../asset/garden.ply");
-	obj2->Init();
+	//auto obj2 = std::make_shared<GaussianPoints>(shared_from_this(), DirectX::XMFLOAT3{0.0f,0.0f,0.0f}, DirectX::XMFLOAT3{1.0f,1.0f,1.0f}, DirectX::XMFLOAT3{0.0f,0.0f,0.0f}, "../asset/garden.ply");
+	//obj2->Init();
 
 	auto camera = std::make_shared<Camera>(shared_from_this(), 90,0.1,100, DirectX::XMFLOAT3{ 0,0,0 }, DirectX::XMFLOAT3{ -90,0,0 });
 	camera->Init();
